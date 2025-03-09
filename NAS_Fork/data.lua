49fe1fe165c3fa91e701bb6903602d5033926e07
Peter <p.eckelmann@gmail.com>
2025-03-09 11:07:38 -0500

    Reset reseach cost & time to same as original mod - updated uranium-235 cost to match SA recipe

--- NAS_Fork/data.lua
+++ NAS_Fork/data.lua
@@ -77,14 +77,9 @@ data:extend(
 					{"military-science-pack", 1},
 					{"space-science-pack", 1},
 					{"utility-science-pack", 1},
-					{"production-science-pack", 1},
-					{"agricultural-science-pack", 1},
-					{"metallurgic-science-pack", 1},
-					{"electromagnetic-science-pack", 1},
-					{"cryogenic-science-pack", 1},
 				},
-				time = 60,
-				count = 10000
+				time = 15,
+				count = 1000
 			},
 			order = "d-e-f"
 		}
