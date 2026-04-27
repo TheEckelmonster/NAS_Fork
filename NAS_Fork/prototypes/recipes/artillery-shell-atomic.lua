6338e112b2a95c96e842365385771da0c06f0fa7
TheEckelmonster <>
2026-04-27 08:39:35 -0500

    feature/0.3.x Added missing recipe localised string and icon(s); added a screenshot of the startup settings to the repo

--- NAS_Fork/prototypes/recipes/artillery-shell-atomic.lua
+++ NAS_Fork/prototypes/recipes/artillery-shell-atomic.lua
@@ -1,10 +1,32 @@
-local Data_Utils = require("data-utils")
+local Data_Utils = require("__TheEckelmonster-core-library__.libs.utils.data-utils")
+local Startup_Settings_Constants = require("settings.startup.startup-settings-constants")
+
+local __Data_Utils = require("data-utils")
+
 local setting = "ARTILLERY_SHELL_ATOMIC"
 
+local icons =
+    Data_Utils.get_startup_setting({ setting = Startup_Settings_Constants.settings[setting .. "_ORIGINAL_ICON"].name, })
+and
+    {
+        {
+            icon = "__NAS_Fork__/graphics/artillery-shell-nuclear.png",
+            icon_size = 32,
+        },
+    }
+or
+    {
+        {
+            icon = "__NAS_Fork__/graphics/icons/artillery-shell-nuclear.png",
+            icon_size = 64,
+        },
+    }
+
 data:extend({
-    Data_Utils.create_recipe({
+    __Data_Utils.create_recipe({
         setting_name = setting,
         name = "artillery-shell-nuclear",
+        icons = icons,
         enabled = false,
         subgroup = "ammo",
         order = "d[explosive-cannon-shell]-d[artillery]2",
